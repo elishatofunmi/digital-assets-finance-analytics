0564ba79eef771d51755fa298a4544625e7841ef
elishatofunmi <elishatofunmi@gmail.com>
2022-11-24 20:18:52 +0100

    int not string

--- stream_etl.py
+++ stream_etl.py
@@ -4,7 +4,7 @@ import os, sys
 
 provider_url = "https://mainnet.infura.io/v3/" + config("project_id")
 kafka_id = config("etl_bootstrap_server")
-batch_size = config("batch_size")
+batch_size = int(config("batch_size"))
 command = "ethereumetl stream --provider-uri %s -e block,transaction,token_transfer --output=kafka/%s --batch-size %i" % (provider_url, kafka_id, batch_size)
 
 
