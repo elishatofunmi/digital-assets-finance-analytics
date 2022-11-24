5482a4342098a4fee00627eeddd9609c93e64d5c
elishatofunmi <elishatofunmi@gmail.com>
2022-11-24 20:17:36 +0100

    consumer and streaming etl

--- consume_etl.py
+++ consume_etl.py
@@ -1,14 +1,15 @@
 from json import loads
 from kafka import KafkaConsumer
 from kafka import TopicPartition
+from decouple import config
 
 consumer = KafkaConsumer(
-     bootstrap_servers=['localhost:9092'], 
+     bootstrap_servers=[config("etl_bootstrap_server")], 
      auto_offset_reset='latest',
      enable_auto_commit=True,
      metadata_max_age_ms=5000)
      #group_id='rectvision01')
-consumer.assign([TopicPartition('blocks', 0)])
+consumer.assign([TopicPartition(config("etl_topic"), 0)])
 
 for message in consumer:
  print(f"{message.value}")
