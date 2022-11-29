2d1aca20f540491c14cd18f9411604369e6d64eb
elishatofunmi <elishatofunmi@gmail.com>
2022-11-29 13:59:32 +0100

    print i

--- src/download_streaming_etl.py
+++ src/download_streaming_etl.py
@@ -144,6 +144,7 @@ tokens_dir = str(uuid.uuid4()) + "export_tokens.csv"
 if __name__ == "__main__":
     print("running....")
     for i, (blocks, transactions, tokens) in enumerate(zip(consume_etl_blocks.consumer, consume_etl_transactions.consumer, consume_etl_tokens.consumer)):
+        print(i)
         if i < 20:
             b_value = loads(blocks.value.decode('utf-8'))
             t_value = loads(transactions.value.decode('utf-8'))
