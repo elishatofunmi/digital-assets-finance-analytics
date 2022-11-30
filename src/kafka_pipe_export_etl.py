7f457dc6f55b4f4f4136fbcc12133d829f512d63
elishatofunmi <elishatofunmi@gmail.com>
2022-11-30 14:24:51 +0100

    kafka export

--- src/kafka_pipe_export_etl.py
+++ src/kafka_pipe_export_etl.py
@@ -23,11 +23,12 @@ class kafka_pipe_etl:
     def __init__(self):
         return 
 
-    def kafka_stream_batches(self, data1, data2):
-        for data_one, data_two in zip (pd.read_csv(data1).iterrows(), pd.read_csv(data2).iterrows()):
+    def kafka_stream_batches(self, data1, data2, data3):
+        for data_one, data_two, data_three in zip (pd.read_csv(data1).iterrows(), pd.read_csv(data2).iterrows(), pd.read_csv(data2).iterrows()):
             send_data = {
                 "block": json.loads(json.dumps(data_one, indent=4, cls=MyJsonEncoder)),
-                "transaction": json.loads(json.dumps(data_two, indent=4, cls=MyJsonEncoder))
+                "transaction": json.loads(json.dumps(data_two, indent=4, cls=MyJsonEncoder)),
+                "tokens": json.loads(json.dumps(data_three, indent=4, cls=MyJsonEncoder))
             }
             etl_kpro.produce(send_data)
             print("sending data: ", send_data)
@@ -43,9 +44,15 @@ class kafka_pipe_etl:
 
 
 if __name__ == "__main__":
-    transaction_dir = "exports/bf1d828a-212f-405f-837f-ae90032fac28_transactions.csv"
-    block_dir = "exports/d135d272-f322-4637-abed-0210c3ab7feb_blocks.csv"
+    blocks = "exports/86647c62-99b3-47ab-97da-000315df9618export_blocks.csv"
+    transactions = "exports/73005056-fbe4-4786-bdd4-8138ee87eb3aexport_transactions.csv"
+    tokens = "exports/bc43eed9-4d43-4ea7-838a-34c8043bf1e0export_tokens.csv"
 
+    # steam csv data for processing
     kpe = kafka_pipe_etl()
-    kpe.kafka_stream_batches(transaction_dir, block_dir)
+    kpe.kafka_stream_batches(transactions, blocks, tokens)
 
+    # Flush Csv from memory
+    # kpe.flush_data(blocks)
+    # kpe.flush_data(transactions)
+    # kpe.flush_data(tokens)
